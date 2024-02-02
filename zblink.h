c61a6a08dfbdae4ba9545cd065503ff1e9be7d3f
Serhii Kuzmychov <ku3cmich@gmail.com>
2024-02-02 17:40:47 +0200

    sync

--- zblink.h
+++ zblink.h
@@ -5,6 +5,10 @@
 #define ZBLINK_LED led0
 #endif
 
+#define SET_LED() gpio_pin_set_dt(&led0, 1)
+#define RES_LED() gpio_pin_set_dt(&led0, 0)
+#define TOGGLE_LED() gpio_pin_toggle_dt(&led0)
+
 void th_zblink();
 #define DEFINE_THREAD_ZBLINK() K_THREAD_DEFINE(th_zblink_id, 512, th_zblink, NULL, NULL, NULL, 5, 0, 0)
 
